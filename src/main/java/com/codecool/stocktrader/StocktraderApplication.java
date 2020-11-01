d7de22807565bf9bff328bc8a45fe0792aa8c969
pFuzzy <p.fazii888@gmail.com>
2020-11-01 07:47:36 -0800

    initial commit

---
+++ src/main/java/com/codecool/stocktrader/StocktraderApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.stocktrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocktraderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StocktraderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/stocktrader/StocktraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.stocktrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocktraderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
